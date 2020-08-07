a962490295d48a5c919f793365ba9e8acb37bb01
hjoelh <jhollingsmith@gmail.com>
2020-08-07 10:39:46 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './todos.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'bacon'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos
+    })
+  }
+
+
+  render () {
+    return (
+    <div className="todo-app container">
+
+    <h1 className='center blue-text'>Todo's</h1>
+
+    <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+
+
+    
+
     </div>
-  );
-}
+    )
+  } }
 
 export default App;
